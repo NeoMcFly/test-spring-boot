60b73c6c185e23f4107b88f301b0f0d208aa0727
theunissenb <benoit.theunissen@cgi.com>
2015-11-06 15:33:07 +0100

    del comments

--- src/main/java/neomcfly/test/spring/boot/Main.java
+++ src/main/java/neomcfly/test/spring/boot/Main.java
@@ -7,15 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
-
-		// AnnotationConfigApplicationContext ctx = new
-		// AnnotationConfigApplicationContext();
-		//
-		// ctx.register(ConfigurationContext.class);
-		// ctx.refresh();
-
 		SpringApplication.run(Main.class, args);
-
 	}
 
 }
